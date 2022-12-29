691a513d9efefb8e3fe4ad97a57f56d1d36cf9e5
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-12-29 13:25:01 +0100

    comments and clean up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// ========== config & consts ========== //
 //==> config og variabler -- det behøver du som sådan ikke forholde dig til
 const express = require("express");
 const cors = require("cors");
@@ -24,13 +25,17 @@ app.use(
 
 //==> det vigtige begynder her
 
+// ========== routes & endpoints ========== //
+
+// GET Route "/"
 app.get("/", (request, response) => {
     response.send("Node Express Users API! Read users from at http://localhost:3000/users"); // det her vises hvis du kører http://localhost:3000 i browser. Vi bruger den som sådan ikke til noget :)
 });
 
+// GET Route "/users"
 // get all users from database
 app.get("/users", (request, response) => {
-    const query = "SELECT * FROM users_data;";
+    const query = "SELECT * FROM users_data;"; // sql query to select all from the table users_data
     mysqlConnection.query(query, (error, results, fields) => {
         if (error) {
             console.log(error);
@@ -40,10 +45,11 @@ app.get("/users", (request, response) => {
     });
 });
 
+// GET Route "/users/:id"
 // get one user from database by given id
 app.get("/users/:id", (request, response) => {
     const id = request.params.id;
-    const query = `SELECT * FROM users_data WHERE id=${id};`;
+    const query = `SELECT * FROM users_data WHERE id=${id};`; // sql query
 
     mysqlConnection.query(query, (error, results, fields) => {
         console.log(results);
@@ -55,10 +61,11 @@ app.get("/users/:id", (request, response) => {
     });
 });
 
+// POST Route "/users"
 // create user
 app.post("/users", (request, response) => {
     const newUser = request.body;
-    const query = `INSERT INTO users_data(firstName, lastName, image, age, email, gender) values("${newUser.firstName}", "${newUser.lastName}", "${newUser.image}", "${newUser.age}", "${newUser.email}", "${newUser.gender}");`;
+    const query = `INSERT INTO users_data(firstName, lastName, image, age, email, gender) values("${newUser.firstName}", "${newUser.lastName}", "${newUser.image}", "${newUser.age}", "${newUser.email}", "${newUser.gender}");`; // sql query
 
     mysqlConnection.query(query, (error, results, fields) => {
         if (error) {
@@ -69,11 +76,12 @@ app.post("/users", (request, response) => {
     });
 });
 
-// UPDATE: update existing user
+// PUT Route "/users/:id"
+// update existing user by given id
 app.put("/users/:id", (request, response) => {
     const id = request.params.id;
     const user = request.body;
-    const query = `UPDATE users_data SET name="${user.name}", mail="${user.mail}", title="${user.title}", image="${user.image}" WHERE id=${id};`;
+    const query = `UPDATE users_data SET name="${user.name}", mail="${user.mail}", title="${user.title}", image="${user.image}" WHERE id=${id};`; // sql query
 
     mysqlConnection.query(query, (error, results, fields) => {
         if (error) {
@@ -84,10 +92,11 @@ app.put("/users/:id", (request, response) => {
     });
 });
 
-// DELETE: delete user
+// DELETE Route "/users/:id"
+// delete user by given id
 app.delete("/users/:id", (request, response) => {
     const id = request.params.id;
-    const query = `DELETE FROM users_data WHERE id=${id};`;
+    const query = `DELETE FROM users_data WHERE id=${id};`; // sql query
 
     mysqlConnection.query(query, (error, results, fields) => {
         if (error) {
@@ -98,6 +107,7 @@ app.delete("/users/:id", (request, response) => {
     });
 });
 
+// binds and listens for connections on the specified host and port
 app.listen(port, () => {
     console.log(`Node.js REST API listening at http://localhost:${port}`);
 });
--- database.js
+++ database.js
@@ -1,23 +1,16 @@
-const mysql = require("mysql2");
-// Read the environment variables
-require('dotenv').config();
+const mysql = require("mysql2"); // using mysql2 - an install npm library
+require("dotenv").config(); // Read the environment variables
 
+// using the variables from the .env file
+// and creates the connection to database
 const mysqlConnection = mysql.createConnection({
     host: process.env.HOST,
     port: process.env.DATABASE_PORT,
     user: process.env.MYSQL_USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
-    multipleStatements: true,
+    multipleStatements: true
 });
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("Connected");
-    } else {
-        console.log(err);
-        console.log("Connection Failed");
-    }
-});
-
+// exports database connection
 module.exports = mysqlConnection;
